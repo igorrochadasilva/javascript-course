c2cffb2525af8d733bf2b3687dab902a62b10ae8
igor rocha <igot082011@hotmail.com>
2019-02-13 11:55:40 -0200

    created new files

--- arrays/imperativoVSdeclarativo.js
+++ arrays/imperativoVSdeclarativo.js
@@ -15,6 +15,7 @@ console.log(total1 / alunos.length)
 // declarativo
 const getNota = aluno => aluno.nota
 const soma = (total, atual) => total + atual
+const subtracao = (total, atual) => total - atual
 
 const total2 = alunos.map(getNota).reduce(soma)
 console.log(total2 / alunos.length)
