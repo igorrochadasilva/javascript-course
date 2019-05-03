491b49e388594b627377eb36bf768a657466eafd
IGOR ROCHA <igor082011@hotmail.com>
2019-05-03 10:47:41 -0300

    created new files

---
+++ esnext/revisao2.js
@@ -0,0 +1,30 @@
+//arrow function - anonima com return implicito
+ const soma = (a, b) => a + b
+ console.log(soma(5,6))
+
+
+ //arrow function (this)
+ const lexico1 = () => console.log(this === exports)
+ const lexico2 = lexico1.bind({})
+ lexico1()
+ lexico2()
+
+
+ //parametro default
+
+ function log(texto = 'Node'){
+     console.log(texto)
+ }
+
+ log()
+ log('Sou mais forte')
+
+
+ //operador rest
+ function total(...numeros){
+     let total = 0
+     numeros.forEach(n => total += n)
+     return total
+ }
+
+console.log(total(2, 3, 4, 5))
